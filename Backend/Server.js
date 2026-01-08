93b9b7d2720ab94c060f39a461e4e7bc990215c0
Kirat <dhimankirat25@gmail.com>
2026-01-08 22:29:47 +0530

    Backend

--- Backend/Server.js
+++ Backend/Server.js
@@ -50,4 +50,12 @@ app.post('/', async (req, res) => {
   }
 })
 
-export default app;
+const PORT = process.env.PORT || 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+export default app
